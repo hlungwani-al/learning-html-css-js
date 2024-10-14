d99af9bc2573d41145bfb7f42e9e3e08622f9829
hlungwani-al <hlungwani.al6@gmail.com>
2024-10-14 13:54:40 +0200

    sone js buttons added

---
+++ main.js
@@ -0,0 +1,12 @@
+function changeTerm1() {
+    document.getElementById('term1').innerHTML = 'The first term';
+    document.getElementById('term1').style.color = 'blue';
+}
+function changeTerm2(){
+    document.getElementById('term2').innerHTML = 'The second term';
+    document.getElementById('term2').style.color = 'tomato';
+}
+function changeTerm3(){
+    document.getElementById('term3').innerHTML ='The third term';
+    document.getElementById('term3').style.color = 'green';
+}
